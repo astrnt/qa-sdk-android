92467f6bee9f7274fb8f6e2402c2e7c3a9823542
Deni Rohimat <rohimat@astrnt.co>
2018-04-20 15:51:23 +0700

    fix: uncomment observer after register API done

--- qasdk/src/main/java/co/astrnt/qasdk/core/RegisterObserver.java
+++ qasdk/src/main/java/co/astrnt/qasdk/core/RegisterObserver.java
@@ -3,6 +3,10 @@ package co.astrnt.qasdk.core;
 import co.astrnt.qasdk.dao.InterviewApiDao;
 import co.astrnt.qasdk.dao.InterviewResultApiDao;
 
+import static co.astrnt.qasdk.type.InterviewType.CLOSE_INTERVIEW;
+import static co.astrnt.qasdk.type.InterviewType.CLOSE_SECTION;
+import static co.astrnt.qasdk.type.InterviewType.CLOSE_TEST;
+
 /**
  * Created by deni rohimat on 12/04/18.
  */
@@ -10,20 +14,20 @@ public abstract class RegisterObserver extends MyObserver<InterviewResultApiDao>
 
     @Override
     public void onApiResultOk(InterviewResultApiDao resultApiDao) {
-//        switch (resultApiDao.getInterview().getType()) {
-//            case CLOSE_INTERVIEW:
-//                onInterviewType(resultApiDao.getInterview());
-//                break;
-//            case CLOSE_TEST:
-//                onTestType(resultApiDao.getInterview());
-//                break;
-//            case CLOSE_SECTION:
-//                onSectionType(resultApiDao.getInterview());
-//                break;
-//            default:
-//                onApiResultError(resultApiDao.getMessage(), "error");
-//                break;
-//        }
+        switch (resultApiDao.getInterview().getType()) {
+            case CLOSE_INTERVIEW:
+                onInterviewType(resultApiDao.getInterview());
+                break;
+            case CLOSE_TEST:
+                onTestType(resultApiDao.getInterview());
+                break;
+            case CLOSE_SECTION:
+                onSectionType(resultApiDao.getInterview());
+                break;
+            default:
+                onApiResultError(resultApiDao.getMessage(), "error");
+                break;
+        }
     }
 
     public abstract void onInterviewType(InterviewApiDao interview);

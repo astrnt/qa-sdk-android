5bac9ae585ee4d6ba2f1a827f7c402ad4d917fef
Deni Rohimat <rohimat@astrnt.co>
2018-05-28 11:27:43 +0700

    add interview and type test for checking

--- qasdk/src/main/java/co/astrnt/qasdk/type/InterviewType.java
+++ qasdk/src/main/java/co/astrnt/qasdk/type/InterviewType.java
@@ -3,11 +3,13 @@ package co.astrnt.qasdk.type;
 public class InterviewType {
 
     public static final String CLOSE = "close";
+    public static final String INTERVIEW = "interview";
     public static final String CLOSE_INTERVIEW = "close interview";
     public static final String CLOSE_TEST = "close test";
     public static final String CLOSE_SECTION = "close section";
     public static final String OPEN = "open";
     public static final String OPEN_INTERVIEW = "open interview";
+    public static final String TEST = "test";
     public static final String OPEN_TEST = "open test";
     public static final String OPEN_SECTION = "open section";
 }

949ec65e4f210de05958274c39f49c231c1428a0
Deni Rohimat <rohimat@astrnt.co>
2019-11-06 10:27:29 +0700

    update manual options

--- qasdk/src/main/java/co/astrnt/qasdk/type/TestType.java
+++ qasdk/src/main/java/co/astrnt/qasdk/type/TestType.java
@@ -3,5 +3,6 @@ package co.astrnt.qasdk.type;
 public class TestType {
     public static final String MCQ = "test";
     public static final String FREE_TEXT = "freetext";
+    public static final String MANUAL = "manual_options_for_test";
 
 }

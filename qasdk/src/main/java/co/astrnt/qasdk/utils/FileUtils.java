c3ac67c982e890df3c2b25c4e45e99bb2cbbae32
Farid <farid@astrnt.co>
2021-04-09 10:58:04 +0700

    add log create folder

--- qasdk/src/main/java/co/astrnt/qasdk/utils/FileUtils.java
+++ qasdk/src/main/java/co/astrnt/qasdk/utils/FileUtils.java
@@ -6,6 +6,7 @@ import android.os.Environment;
 import java.io.File;
 import java.io.IOException;
 
+import co.astrnt.qasdk.dao.LogDao;
 import timber.log.Timber;
 
 public class FileUtils {
@@ -25,24 +26,39 @@ public class FileUtils {
         return subDirectory;
     }
 
-    public static File makeAndGetSubDirectoryDownload(String appName, String subFolderName, String jobNameFolder) {
+    public static File makeAndGetSubDirectoryDownload(String appName, String subFolderName, String jobNameFolder, String interviewCode) {
         File appNameDirectory = new File(Environment.getExternalStorageDirectory(),appName);
 
         if (!appNameDirectory.exists()) {
             Timber.e("Create app folder in root");
             appNameDirectory.mkdirs();
+            LogUtil.addNewLog(interviewCode,
+                    new LogDao("CheatSheet Info",
+                            "Create app folder in root"
+                    )
+            );
         }
 
         File subDirectory = new File(appNameDirectory.getAbsolutePath(), subFolderName);
         if (!subDirectory.exists()) {
             Timber.e("Create sub folder in AstronautQ&A");
             subDirectory.mkdirs();
+            LogUtil.addNewLog(interviewCode,
+                    new LogDao("CheatSheet Info",
+                            "Create sub folder "+subFolderName
+                    )
+            );
         }
 
         File jobName = new File(subDirectory.getAbsolutePath(), jobNameFolder);
         if (!jobName.exists()) {
             Timber.e("Create sub folder in job name");
             jobName.mkdirs();
+            LogUtil.addNewLog(interviewCode,
+                    new LogDao("CheatSheet Info",
+                            "Create sub folder in job name "+jobNameFolder
+                    )
+            );
         }
         return jobName;
     }

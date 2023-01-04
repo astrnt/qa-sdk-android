e9e0f864698bf5cc954e3525224ad7c06bc42052
farid <farid@astrnt.co>
2023-01-04 17:22:46 +0700

    AS-3162 #comment add class showkeyboard #time 30m

---
+++ qasdk/src/main/java/co/astrnt/qasdk/event/ShowKeyboard.java
@@ -0,0 +1,12 @@
+package co.astrnt.qasdk.event;
+
+public class ShowKeyboard {
+    public boolean isShown = false;
+
+    public ShowKeyboard(boolean isShown) {
+        this.isShown = isShown;
+    }
+
+    public ShowKeyboard() {
+    }
+}
